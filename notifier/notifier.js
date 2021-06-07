f44b0ebbe9b8191419ba015a2765d07d64c306ce
Stephen Mahon <stemahon@gmail.com>
2021-06-07 11:21:00 -0500

    Notifier service backend

---
+++ notifier/notifier.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const notifier = require('node-notifier');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 9000;
+
+app.use(bodyParser.json());
+app.get('/health', (req, res) => res.status(200).send());
+app.post('/notify', (req, res) => {
+    notify(req.body, reply => res.send(reply))
+});
+
+app.listen(port, () => console.log(`server is up and running on port: ${port}`));
+
+const notify = ({title, message}, cb) => {
+    notifier.notify(
+        {
+            title: title || 'Unknown title',
+            message: message || 'Unknown message',
+            icon: path.join(__dirname, "coffee.png"),
+            sound: true,
+            wait: true,
+            reply: true,
+            actions: ["Completed?", "Reply"],
+            timeout: 15,
+        },
+        (err, response, reply) => {
+            cb(reply)
+        }
+    );
+};
